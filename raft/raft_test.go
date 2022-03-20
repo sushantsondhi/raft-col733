0ee84e6f06d4c04e0bf34f53917af950ac6f5a65
sarthak <sarthakagrawal8@gmail.com>
2022-03-20 12:49:25 +0530

    Make test setup more idiomatic

--- raft/raft_test.go
+++ raft/raft_test.go
@@ -7,39 +7,54 @@ import (
 	"github.com/sushantsondhi/raft-col733/common"
 	"github.com/sushantsondhi/raft-col733/persistent"
 	"github.com/sushantsondhi/raft-col733/rpc"
+	"os"
+	"path/filepath"
 	"testing"
 	"time"
 )
 
-func Test_SimpleElection(t *testing.T) {
-	serverDetails := []common.Server{
-		{
-			ID:         uuid.New(),
-			NetAddress: ":12345",
-		},
-		{
-			ID:         uuid.New(),
-			NetAddress: ":12346",
-		},
-		{
-			ID:         uuid.New(),
-			NetAddress: ":12347",
-		},
+func makeRaftCluster(t *testing.T, configs ...common.ClusterConfig) (servers []*RaftServer) {
+	for i := range configs {
+		logstore, err := persistent.CreateDbLogStore(fmt.Sprintf("logstore-%v.db", configs[i].Cluster[i].ID))
+		assert.NoError(t, err)
+		pstore, err := persistent.NewPStore(fmt.Sprintf("pstore-%v.db", configs[i].Cluster[i].ID))
+		assert.NoError(t, err)
+		raftServer := NewRaftServer(configs[i].Cluster[i], configs[i], nil, logstore, pstore, &rpc.Manager{})
+		assert.NotNil(t, raftServer)
+		servers = append(servers, raftServer)
 	}
-	var servers []*RaftServer
-	cluster := common.ClusterConfig{
-		Cluster:          serverDetails,
+	return
+}
+
+func cleanupDbFiles() {
+	matches, err := filepath.Glob("*.db")
+	if err != nil {
+		panic(err)
+	}
+	for _, match := range matches {
+		os.Remove(match)
+	}
+}
+
+func generateClusterConfig(n int) common.ClusterConfig {
+	var servers []common.Server
+	for i := 0; i < n; i++ {
+		servers = append(servers, common.Server{
+			ID:         uuid.New(),
+			NetAddress: common.ServerAddress(fmt.Sprintf(":%d", 12345+i)),
+		})
+	}
+	return common.ClusterConfig{
+		Cluster:          servers,
 		HeartBeatTimeout: 50 * time.Millisecond,
 		ElectionTimeout:  200 * time.Millisecond,
 	}
+}
 
-	for i := range serverDetails {
-		logStore, err := persistent.CreateDbLogStore("logstore-" + serverDetails[i].ID.String() + ".db")
-		assert.NoError(t, err)
-		persistentStore, err := persistent.NewPStore("persistentstore-" + serverDetails[i].ID.String() + ".db")
-		server := NewRaftServer(serverDetails[i], cluster, nil, logStore, persistentStore, &rpc.Manager{})
-		servers = append(servers, server)
-	}
+func Test_SimpleElection(t *testing.T) {
+	clusterConfig := generateClusterConfig(3)
+	servers := makeRaftCluster(t, clusterConfig, clusterConfig, clusterConfig)
+	t.Cleanup(cleanupDbFiles)
 
 	for i := 0; i < 20; i++ {
 		leaders := make(map[int64][]uuid.UUID)
