8e749eaf51aab418f511c85ae1d13665f04eab10
Sarthak Agrawal <sarthakagrawal8@gmail.com>
2022-03-22 11:30:21 +0530

    (Ramneek) Idempotent client requests (#12)
    * - Added check to prevent committing entries from previous terms
    - Added idempotence to client requests.
    
    * - Completed idempotence implementation.
    - Removed transactionId from logEntry
    - Added test for idempotence.
    
    * Fix a few things
    
    Co-authored-by: RamneekSingh24 <ramneeksingh2001@gmail.com>

--- kvstore/fsm_test.go
+++ kvstore/fsm_test.go
@@ -2,6 +2,7 @@ package kvstore_test
 
 import (
 	"encoding/json"
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/sushantsondhi/raft-col733/common"
 	"github.com/sushantsondhi/raft-col733/kvstore"
@@ -11,9 +12,10 @@ import (
 func TestKeyValFSM_Apply(t *testing.T) {
 	setMarshaller := func(key, val string) common.LogEntry {
 		bytes, err := json.Marshal(kvstore.Request{
-			Type: kvstore.Set,
-			Key:  key,
-			Val:  val,
+			Type:          kvstore.Set,
+			Key:           key,
+			Val:           val,
+			TransactionId: uuid.New(),
 		})
 		assert.NoError(t, err)
 		return common.LogEntry{
@@ -22,8 +24,9 @@ func TestKeyValFSM_Apply(t *testing.T) {
 	}
 	getMarshaller := func(key string) common.LogEntry {
 		bytes, err := json.Marshal(kvstore.Request{
-			Type: kvstore.Get,
-			Key:  key,
+			Type:          kvstore.Get,
+			Key:           key,
+			TransactionId: uuid.New(),
 		})
 		assert.NoError(t, err)
 		return common.LogEntry{
@@ -65,4 +68,65 @@ func TestKeyValFSM_Apply(t *testing.T) {
 	bytes, err = fsm.Apply(getMarshaller("a"))
 	assert.NoError(t, err)
 	assert.EqualValues(t, bytes, []byte("2"))
+}
+
+func TestKeyValFSM_Idempotence(t *testing.T) {
+
+	setMarshaller := func(key, val string, transId uuid.UUID) common.LogEntry {
+		bytes, err := json.Marshal(kvstore.Request{
+			Type:          kvstore.Set,
+			Key:           key,
+			Val:           val,
+			TransactionId: transId,
+		})
+		assert.NoError(t, err)
+		return common.LogEntry{
+			Data: bytes,
+		}
+	}
+	getMarshaller := func(key string, transId uuid.UUID) common.LogEntry {
+		bytes, err := json.Marshal(kvstore.Request{
+			Type:          kvstore.Get,
+			Key:           key,
+			TransactionId: transId,
+		})
+		assert.NoError(t, err)
+		return common.LogEntry{
+			Data: bytes,
+		}
+	}
+
+	fsm := kvstore.NewKeyValFSM()
+	var bytes []byte
+	var err error
+
+	t_id1 := uuid.New()
+	bytes, err = fsm.Apply(setMarshaller("a", "1", t_id1))
+	assert.NoError(t, err)
+	assert.Nil(t, bytes)
+
+	bytes, err = fsm.Apply(getMarshaller("a", uuid.New()))
+	assert.NoError(t, err)
+	assert.EqualValues(t, bytes, []byte("1"))
+
+	t_id2 := uuid.New()
+	bytes, err = fsm.Apply(setMarshaller("a", "2", t_id2))
+	assert.NoError(t, err)
+	assert.Nil(t, bytes)
+
+	// get should return the new value
+	bytes, err = fsm.Apply(getMarshaller("a", uuid.New()))
+	assert.NoError(t, err)
+	assert.EqualValues(t, bytes, []byte("2"))
+
+	// Repeat an old transaction
+	bytes, err = fsm.Apply(setMarshaller("a", "1", t_id1))
+	assert.NoError(t, err)
+	assert.Nil(t, bytes)
+
+	// Should not affect latest value
+	bytes, err = fsm.Apply(getMarshaller("a", uuid.New()))
+	assert.NoError(t, err)
+	assert.EqualValues(t, bytes, []byte("2"))
+
 }
